e1faed42a7724c02e5cb2988f62ad400e94c1c26
Raj singh <raj@caleida.io>
2026-05-13 20:47:03 +0530

    feat: optimized assets size

--- lib/base/constants/strings/asset_constants.dart
+++ lib/base/constants/strings/asset_constants.dart
@@ -10,7 +10,7 @@ class AssetConstants {
   static const String catPeekUpSideDownImage = "$_images/cat_peek_usd.png";
   static const String catPeekImage = "$_images/cat_peek.png";
   static const String catInValley = "$_images/cat_in_valley.jpeg";
-  static const String catInValleyWide = "$_images/cat_in_valley_wide.png";
+  static const String catInValleyWide = "$_images/cat_in_valley_wide.jpg";
   static const String catInValleyWideDark =
-      "$_images/cat_in_valley_wide_dark.png";
+      "$_images/cat_in_valley_wide_dark.jpg";
 }
