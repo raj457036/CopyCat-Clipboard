d0eecd2a1802ff1e9d4f72936b965ec2c198acc8
Raj <rs457036@gmail.com>
2026-05-29 09:18:11 +0530

    feat: merge quickpop windows

---
+++ packages/quick_paste_popup/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <quick_paste_popup/quick_paste_popup_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  QuickPastePopupPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("QuickPastePopupPluginCApi"));
+}
