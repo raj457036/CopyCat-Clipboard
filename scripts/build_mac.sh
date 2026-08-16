705c40ffc6d9430cad916ba3755ae38c2b8c86d1
Raj singh <raj@caleida.io>
2026-08-16 13:23:50 +0530

    feat: gh action for release

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,2 +1,10 @@
-flutter build macos --verbose --tree-shake-icons --dart-define-from-file="local/prod.json" --obfuscate --split-debug-info=./build_symbols
-# && create-dmg --overwrite --identity="Apple Development: Raj Singh (X57UNFWL7W)" build/macos/Build/Products/Release/CopyCat.app
+flutter build macos --verbose \
+  --tree-shake-icons \
+  --dart-define-from-file="local/prod.json" \
+  --obfuscate \
+  --split-debug-info=./build_symbols \
+&& codesign --remove-signature \
+  build/macos/Build/Products/Release/CopyCat.app
+&& ditto -c -k --keepParent \
+  build/macos/Build/Products/Release/CopyCat.app \
+  CopyCat-macOS.zip
