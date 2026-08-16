7485f6d9fa49118e6504ec0a4f2b576c19e58502
Raj singh <raj@caleida.io>
2026-08-16 13:38:07 +0530

    feat: update build script for mac

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -2,9 +2,9 @@ flutter build macos --verbose \
   --tree-shake-icons \
   --dart-define-from-file="local/prod.json" \
   --obfuscate \
-  --split-debug-info=./build_symbols \
-&& codesign --remove-signature \
-  build/macos/Build/Products/Release/CopyCat.app
-&& ditto -c -k --keepParent \
+  --split-debug-info=./build_symbols && \
+codesign --remove-signature \
+  build/macos/Build/Products/Release/CopyCat.app && \
+ditto -c -k --keepParent \
   build/macos/Build/Products/Release/CopyCat.app \
   CopyCat-macOS.zip
